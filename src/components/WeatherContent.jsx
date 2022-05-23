d5e4f8bb2ad0bd452d69186c37f421c7c0c72125
bits-wa <bitswa@pm.me>
2022-05-23 01:45:43 -0300

    new updates

--- src/components/WeatherContent.jsx
+++ src/components/WeatherContent.jsx
@@ -1,32 +1,35 @@
 import { useContext } from 'react';
 import { ContextApi } from '../contexts/ContextApi';
+import RenderIf from '../utilities/RenderIf';
 
 export default function WeatherContent() {
 
   const { list } = useContext(ContextApi);
 
   return (
-    <section className='c-content'>
-      <div className='c-location'>
-        <h1 className='c-location__name'>
-          {list.current && list.location.name}
-        </h1>
-        <h2 className='c-location__temperature'>
-          {list.current && list.current.temp_c}°C
-        </h2>
-      </div>
-      <div className='c-current'>
-        <div>
-          <img src={list.current && list.current.condition.icon} alt=''/>
-          <p>{list.current && list.current.condition.text}</p>
+    <RenderIf isTrue={list.current}>
+      <section className='c-content'>
+        <div className='c-location'>
+          <h1 className='c-location__name'>
+            {list.current && list.location.name}
+          </h1>
+          <h2 className='c-location__temperature'>
+            {list.current && list.current.temp_c}°C
+          </h2>
         </div>
-        <p className='c-current__rain'>
-            Chance of rain: {list.current && list.current.precip_in}%
-        </p>
-        <p className='c-current__wind'>
-          Wind: {list.current && list.current.wind_kph}km/h
-        </p>
-      </div>
-    </section>
+        <div className='c-current'>
+          <div>
+            <img src={list.current && list.current.condition.icon} alt=''/>
+            <p>{list.current && list.current.condition.text}</p>
+          </div>
+          <p className='c-current__rain'>
+              Humidity: {list.current && list.current.humidity}%
+          </p>
+          <p className='c-current__wind'>
+            Wind: {list.current && list.current.wind_kph}km/h
+          </p>
+        </div>
+      </section>
+    </RenderIf>
   )
 }
--- src/contexts/ContextApi.jsx
+++ src/contexts/ContextApi.jsx
@@ -6,15 +6,15 @@ export function ContextApiProvider({children}) {
   const [location, setLocation] = useState('');
   const [list, setList] = useState({});
 
-  const url = `https://api.weatherapi.com/v1/current.json?key=e15301164db64de7884174936222105&q=${location}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=e15301164db64de7884174936222105&q=${location}&days=1&aqi=no&alerts=no`;
 
   const handleSearch = () => {
     if (location) {
       fetch(url)
         .then(promise => promise.json())
         .then(json => {
-          setLocation('')
-          setList(json)
+          setLocation('');
+          setList(json);
         })
         .catch(err => console.log(err));
     }
